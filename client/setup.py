cafbd7e9de014ccabe5b57d4c20b4048bec5111c
paphio-kitsuki <IT.King5963@gmail.com>
2024-05-26 03:01:20 +0900

    change setup and requirements

--- client/setup.py
+++ client/setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 setup(
-    name="blocks_duo_ss_tarou",
-    version="0.0.1",
-    description="smartscape blocks-duo player package",
-    author="Yoshitomi",
+    name="blocks_duo_autonomic_nervous_system",
+    version="1.0.0",
+    description="poor noob blocks-duo player package",
+    author="autonomic_nervous_system",
     packages=find_packages(),
     install_requires=install_requirements,
     entry_points={
         "console_scripts": [
-            "ss_tarou=ss_player.main:main",
+            "A_N_S_noob=ss_player.main:main",
         ]
     },
     classifiers=[
