dea11667efee1fb8858647af218881cc7f0a67a6
Jay Takahashi <qsakura14555@gmail.com>
2024-05-25 12:46:22 +0900

    edit code

--- client/ss_player/PlayerClient.py
+++ client/ss_player/PlayerClient.py
@@ -8,10 +8,9 @@ class PlayerClient:
         self._loop = loop
         self._socket = socket
         self._player_number = player_number
-        self.p1Actions = ['U034', 'B037', 'J266', 'M149', 'O763', 'R0A3', 'F0C6', 'K113', 'T021', 'L5D2', 'G251', 'E291', 'D057', 'A053']
-        self.p2Actions = ['A0AA', 'B098', 'N0A5', 'L659', 'K33B', 'J027', 'E2B9', 'C267', 'U07C', 'M3AD', 'O2BB', 'R41C']
-        self.p1turn = 0
-        self.p2turn = 0
+        self._player_char = 'o' if player_number == 1 else 'x'
+        self._enemy_char = 'x' if player_number == 1 else 'o'
+        self.trun = 0
 
     @property
     def player_number(self) -> int:
@@ -29,24 +28,15 @@ class PlayerClient:
                 raise SystemExit
 
     def create_action(self, board):
-        actions: list[str]
-        turn: int
-
-        if self.player_number == 1:
-            actions = self.p1Actions
-            turn = self.p1turn
-            self.p1turn += 1
-        else:
-            actions = self.p2Actions
-            turn = self.p2turn
-            self.p2turn += 1
-
-        if len(actions) > turn:
-            return actions[turn]
+        actions = ''
+        if self.turn == 0 and self.player_number == 1:
+            return 'Q055'
+        elif self.turn == 0 and self.player_number == 2:
+            return 'Q2AA'
         else:
             # パスを選択
             return 'X000'
-    
+
     @staticmethod
     async def create(url: str, loop: asyncio.AbstractEventLoop) -> PlayerClient:
         socket = await websockets.connect(url)
